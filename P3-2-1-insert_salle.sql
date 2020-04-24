fa2848ba02a27cb3f17c54fdc96ef81ba52dde2c
Yann MOTTOLA <yann.mottola@iut-rodez.fr>
2020-04-24 09:41:52 +0200

    fix #7 fonction P3-2-2 fonction insert_reservation

---
+++ P3-2-1-insert_salle.sql
@@ -0,0 +1,7 @@
+CREATE FUNCTION gds.insert_salle(
+        un_nom VARCHAR(250), 
+        un_nb_personnes_max integer) 
+RETURNS gds.salle AS $$
+        INSERT INTO gds.salle (id, nom, nb_personnes_max, date_creation, date_modification) 
+        VALUES (nextval('gds.salle_seq'),un_nom, un_nb_personnes_max, now(), now()) returning *;
+$$ LANGUAGE SQL;
