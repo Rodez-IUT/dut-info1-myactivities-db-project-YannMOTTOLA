2c7c4be35b6f8e1e722bff545a0cd487f0c86349
Yann MOTTOLA <yann.mottola@iut-rodez.fr>
2020-04-29 10:47:29 +0200

    fix #9 fonction pl_insert_salle

---
+++ P4-1-pl_insert_salle.sql
@@ -0,0 +1,4 @@
+CREATE OR REPLACE FUNCTION gds.pl_insert_salle(un_nom character varying(250), un_nb_personnes_max integer) RETURNS gds.salle AS $$
+INSERT INTO gds.salle (id, nom, nb_personnes_max, date_creation, date_modification)
+VALUES (nextval('gds.salle_seq'), un_nom, un_nb_personnes_max, now(), now()) returning *;
+$$ LANGUAGE SQL;
